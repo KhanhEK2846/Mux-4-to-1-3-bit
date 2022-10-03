3c91b26aadd829e4ba03f8b0742c19d4ced94952
KhanhEK <107037350+KhanhEK2846@users.noreply.github.com>
2022-10-03 16:43:47 +0700

    Update Mux4to1_3bit.v

--- Mux4to1_3bit.v
+++ Mux4to1_3bit.v
@@ -3,8 +3,8 @@ input [2:0]x,y,z,w;
 input [1:0]c;
 output [2:0]o;
 	assign	o =	(c == 2'b00)?x:
-						(c == 2'b01)?y:
-						(c == 2'b10)?z:
-						(c == 2'b11)?w:
-						1'bx;
+			(c == 2'b01)?y:
+			(c == 2'b10)?z:
+			(c == 2'b11)?w:
+			1'bx;
 endmodule
